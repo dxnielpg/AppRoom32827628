74ca386a8eeea6643e4b4bcd80b61ce094e8da30
Daniel Pereira Gonçalves <danielpgo11@gmail.com>
2024-11-27 02:02:39 -0300

    Implementação do repositório

--- app/src/main/java/br/edu/up/rgm32827628/data/ItemsRepository.kt
+++ app/src/main/java/br/edu/up/rgm32827628/data/ItemsRepository.kt
@@ -19,4 +19,12 @@ package br.edu.up.rgm32827628.data
 /**
  * Repository that provides insert, update, delete, and retrieve of [Item] from a given data source.
  */
-interface ItemsRepository
+import kotlinx.coroutines.flow.Flow
+interface ItemsRepository {
+    fun getAllItemsStream(): Flow<List<Item>>
+    fun getItemStream(id: Int): Flow<Item?>
+    suspend fun insertItem(item: Item)
+    suspend fun deleteItem(item: Item)
+    suspend fun updateItem(item: Item)
+}
+
